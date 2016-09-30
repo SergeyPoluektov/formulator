90b83404709c510410dd30aea68675c4ba676737
Sergey Poluektov <spoluer@gmail.com>
2016-09-30 20:47:13 +0300

    Testing info was deleted

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 const formulator = require('./lib/index.js')
 
-// for test
-let dataObj = {
-  bookings: 60,
-  clicks: 5
-}
-let config = {
-	BTR: 'bookings / clicks'
-}
-
-for (let metric in config) {
-	dataObj[metric] = formulator({
-		values: dataObj,
-		formula: config[metric]
-	})
-}
-
-console.log(dataObj.BTR)
-
 module.exports = formulator
