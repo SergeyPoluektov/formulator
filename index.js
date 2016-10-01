b48fea7a585417f9210b28614a3eec41eb6951d0
Sergey Poluektov <spoluer@gmail.com>
2016-10-01 20:53:15 +0300

    Hot fix for raw numbers

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const formulator = require('./lib/index.js')
 
+let dataObj = {
+  bookings: 60,
+  clicks: 5
+}
+let config = {
+    BTR: '30 / 3 * 100'
+}
+
+for (let metric in config) {
+    dataObj[metric] = formulator({
+        values: dataObj,
+        formula: config[metric]
+    })
+}
+
+console.log(dataObj.BTR)
+
 module.exports = formulator
