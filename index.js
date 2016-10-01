8edc7f9c0dc6311df77767c52bec1a07df5bc9ca
Sergey Poluektov <spoluer@gmail.com>
2016-10-01 21:14:53 +0300

    Convert symbol from string to number when add to output

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 const formulator = require('./lib/index.js')
 
-let dataObj = {
-  bookings: 60,
-  clicks: 5
-}
-let config = {
-    BTR: '30 / 3 * 100'
-}
-
-for (let metric in config) {
-    dataObj[metric] = formulator({
-        values: dataObj,
-        formula: config[metric]
-    })
-}
-
-console.log(dataObj.BTR)
-
 module.exports = formulator
